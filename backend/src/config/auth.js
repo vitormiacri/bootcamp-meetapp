4f90cb787ef0d8052c793ba570d3f745a14275e0
Vitor Morelli <vitao.miacri@gmail.com>
2019-07-05 10:47:46 -0300

    Users functions

---
+++ backend/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '6722ef4ef53d2d804329750f2117955a',
+  expiresIn: '7d',
+};
---
+++ backend/src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'mysql',
+  host: 'localhost',
+  username: 'meetapp',
+  password: 'meetapp',
+  database: 'meetapp',
+  port: 3307,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
